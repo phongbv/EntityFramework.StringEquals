d1da1d454d76eb238df99316e4bb892495fe3bec
Bùi Văn Phong <buivan.phong94@gmail.com>
2020-02-11 11:57:54 +0700

    Create QueryableExtensions & StringEqualsVisitor

---
+++ StringEquals/StringEquals/QueryableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEquals
+{
+    public static class QueryableExtensions
+    {
+
+        /// <summary>
+        /// Convert leftOperator == rightOperator to optimize entity framework linq.
+        /// Suppose <paramref name="leftOperator"/> is string constant and <paramref name="rightOperator"/> is entity property
+        /// <para></para>
+        /// <para>If leftOperator is null or empty: rightOperator == null </para>
+        /// <para>If leftOperator is not null and not empty: rightOperator != null && leftOperator == rightOperator</para>
+        /// <para> Call <code>InterceptStringEquals()</code> in last Linq </para>
+        /// </summary>
+        /// <param name="leftOperator"></param>
+        /// <param name="rightOperator"></param>
+        /// <returns></returns>
+        public static bool EqualsExt(this string leftOperator, string rightOperator)
+        {
+            throw new NotImplementedException();
+        }
+        public static IQueryable<T> InterceptWith<T>(this IQueryable<T> source, params ExpressionVisitor[] visitors)
+        {
+            var expression = source.Expression;
+            foreach (var item in visitors)
+            {
+                expression = item.Visit(expression);
+            }
+            if (expression == source.Expression) return source;
+            return source.Provider.CreateQuery<T>(expression);
+        }
+        public static IQueryable<T> InterceptStringEquals<T>(this IQueryable<T> source)
+        {
+            var visitor = new StringEqualsVisitor();
+            return source.InterceptWith(visitor);
+        }
+    }
+
+}
---
+++ StringEquals/StringEquals/StringEqualsVisitor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEquals
+{
+    public class StringEqualsVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.Name == nameof(QueryableExtensions.EqualsExt))
+            {
+                return BuildStringEqualsExpression(node);
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        /// <summary>
+        /// Convert leftOperator == rightOperator to optimize entity framework linq.
+        /// Suppose leftOperator is string constant and rightOperator is entity property
+        /// <para>If leftOperator is null or empty: rightOperator == null </para>
+        /// <para>If leftOperator is not null and not empty: rightOperator != null && leftOperator == rightOperator</para>
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns>
+        /// <para>If leftOperator is null or empty: rightOperator == null </para>
+        /// <para>If leftOperator is not null and not empty: rightOperator != null && leftOperator == rightOperator</para>
+        /// </returns>
+        private Expression BuildStringEqualsExpression(MethodCallExpression node)
+        {
+            var arg0 = node.Arguments[0] is MemberExpression ? node.Arguments[0] as MemberExpression : null;
+            var arg1 = node.Arguments[1] is MemberExpression ? node.Arguments[1] as MemberExpression : null;
+            if (arg0 == null && arg1 == null)
+            {
+                throw new NotSupportedException();
+            }
+            var propertyNode = arg0 != null && arg0.Expression is ConstantExpression ? node.Arguments[1] : node.Arguments[0];
+            var constantNode = arg0 != null && arg0.Expression is ConstantExpression ? arg0 : arg1;
+            var fieldExp = constantNode as MemberExpression;
+            var objectValue = (fieldExp.Expression as ConstantExpression).Value;
+            var compareVal = objectValue.GetType().GetField(fieldExp.Member.Name, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).GetValue(objectValue) + "";
+            var nullExp = Expression.Constant(null);
+            if (string.IsNullOrEmpty(compareVal))
+            {
+                return Expression.Equal(propertyNode, nullExp);
+            }
+            var notNullExp = Expression.NotEqual(propertyNode, nullExp);
+            var propEqCstExp = Expression.Equal(propertyNode, constantNode);
+            return Expression.AndAlso(notNullExp, propEqCstExp);
+        }
+    }
+
+}
